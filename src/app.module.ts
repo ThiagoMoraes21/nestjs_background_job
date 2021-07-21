5a4f447057b5f2918dddf67d899c197e9a6b913f
thiagoMoraes21 <thiago4x70@gmail.com>
2021-07-21 13:43:18 -0300

    add mailer service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,32 @@
 import { CreateUserController } from './create-user/create-user.controller';
 import { Module } from '@nestjs/common';
+import { BullModule } from '@nestjs/bull';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [
-    CreateUserController
-  ],
-  providers: [],
+    imports: [
+        ConfigModule.forRoot(),
+        BullModule.forRoot({
+            redis: {
+                host: 'localhost',
+                port: 6379,
+            },
+        }),
+        MailerModule.forRoot({
+            transport: {
+                host: process.env.MAIL_HOST,
+                port: Number(process.env.MAIL_PORT),
+                auth: {
+                    user: process.env.MAIL_USER,
+                    pass: process.env.MAIL_PASS
+                }
+            }
+        })
+    ],
+    controllers: [
+        CreateUserController
+    ],
+    providers: [],
 })
-export class AppModule {}
+export class AppModule { }
--- src/create-user/create-user.controller.ts
+++ src/create-user/create-user.controller.ts
@@ -1,11 +1,19 @@
 import { CreateUserDto } from './create-user-dto';
 import { Body, Controller, Post } from '@nestjs/common';
+import { MailerService } from '@nestjs-modules/mailer';
 
 @Controller('create-user')
 export class CreateUserController {
 
+    constructor(private mailService: MailerService) {}
+
     @Post()
-    createUser(@Body() createUserDto: CreateUserDto) {
-        return createUserDto;
+    async createUser(@Body() createUser: CreateUserDto): Promise<void> {
+        await this.mailService.sendMail({
+            to: createUser.email,
+            from: 'Equipe Code/Drops <codedropgs@codedrops.com.br>',
+            subject: 'Seja Bem Vindo',
+            text: `Olá, ${createUser.name}, seu cadastro foi realizado com sucesso. Seja bem vindo(a)!`
+        });
     }
 }
